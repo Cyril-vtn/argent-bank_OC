72c94a54b1fc9917af24f20f31d7b836b53b738c
Cyril-vtn <votion.cyril@gmail.com>
2024-07-08 12:33:40 +0200

    chore: Update ProtectedRoute component to use Redux for authentication

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,9 @@
+import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
-// Importez useSelector de react-redux une fois que vous avez configuré Redux
-// import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
-  // Remplacez cette partie par la logique de vérification d'authentification avec Redux
-  const isAuthenticated = true; // useSelector(state => state.user.isAuthenticated);
+  const user = useSelector((state) => state.user);
+  const isAuthenticated = user.token;
   const location = useLocation();
 
   if (!isAuthenticated) {
