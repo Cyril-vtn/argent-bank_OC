0380a7e50a05d88bdff7e05ba300457df82a73f2
Cyril-vtn <votion.cyril@gmail.com>
2024-07-08 17:23:39 +0200

    chore: Update profile page to include edit functionality for user's name

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../styles/profile.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../features/user/userSlice";
+
 export const Profile = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editName, setEditName] = useState({
+    firstName: user.userInfo ? user.userInfo.firstName : "",
+    lastName: user.userInfo ? user.userInfo.lastName : "",
+  });
+
+  useEffect(() => {
+    if (user.userInfo) {
+      setEditName({
+        firstName: user.userInfo.firstName,
+        lastName: user.userInfo.lastName,
+      });
+    }
+  }, [user.userInfo]);
 
   useEffect(() => {
     if (!user.token) {
@@ -33,17 +48,87 @@ export const Profile = () => {
         console.error("There was a problem with your fetch operation:", error);
       });
   }, [user.token, dispatch]);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditName((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleConfirmEdit = () => {
+    if (!editName.firstName || !editName.lastName) {
+      alert("Please fill in both fields");
+      return;
+    }
+    fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify({
+        firstName: editName.firstName,
+        lastName: editName.lastName,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(
+          setUser({
+            userInfo: data.body,
+            token: user.token,
+          })
+        );
+        setIsEditing(false);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          {user.userInfo
-            ? user.userInfo.firstName + " " + user.userInfo.lastName
-            : ""}
-        </h1>
-        <button className="edit-button">Edit Name</button>
+        {isEditing ? (
+          <h1>
+            Welcome back
+            <br />
+            <input
+              className="edit-input"
+              name="firstName"
+              value={editName.firstName}
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              className="edit-input"
+              name="lastName"
+              value={editName.lastName}
+              onChange={handleChange}
+            />
+            <br />
+            <button className="edit-button" onClick={handleConfirmEdit}>
+              Confirm Edit
+            </button>
+          </h1>
+        ) : (
+          <>
+            <h1>
+              Welcome back
+              <br />
+              {user.userInfo
+                ? `${user.userInfo.firstName} ${user.userInfo.lastName}`
+                : ""}
+            </h1>
+            <button className="edit-button" onClick={handleEdit}>
+              Edit Name
+            </button>
+          </>
+        )}
       </div>
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
