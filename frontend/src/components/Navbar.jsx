91d82920d774536406567dc7bf3b5669e70ca93b
Cyril-vtn <votion.cyril@gmail.com>
2024-07-08 13:09:47 +0200

    chore: Update Navbar component to include user authentication and sign out functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import "../styles/navbar.css";
+import { useDispatch, useSelector } from "react-redux";
 import argentBankLogo from "../images/argentBankLogo.png";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../features/user/userSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle, faSignOut } from "@fortawesome/free-solid-svg-icons";
 export const Navbar = () => {
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to={"/"}>
@@ -13,10 +26,23 @@ export const Navbar = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to={"/sign-in"}>
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
+        {user.token ? (
+          <>
+            <Link className="main-nav-item" to="/profile">
+              <FontAwesomeIcon icon={faUserCircle} />
+              {user.userInfo ? user.userInfo.firstName : ""}
+            </Link>
+            <a className="main-nav-item" onClick={signOut}>
+              <FontAwesomeIcon icon={faSignOut} />
+              Sign Out
+            </a>
+          </>
+        ) : (
+          <Link className="main-nav-item" to={"/sign-in"}>
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
